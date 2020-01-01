4ade8f467e7d1a9c2e4fa7c21355c48706508a03
CubeDr <spaceship00@naver.com>
2020-01-01 16:20:58 +0900

    UPG: cari.js uses b-thumbnail

--- front/js/cari.js
+++ front/js/cari.js
@@ -38,50 +38,10 @@ function sendAjax(url){
         if(result.result !== 'ok') return;
         // 데이터가 있으면 결과값 표시
         var randomul = document.getElementById('uls');
-        for(var i=0; i<result.data.length; i++){
-            var lis = document.createElement('li');
-            var as = document.createElement('a');
-            var dls = document.createElement('dl');
-            var dts = document.createElement('dt');
-            var span_id = document.createElement('span');
-            var dds1 = document.createElement('dd');
-            var dds2 = document.createElement('dd');
-            var dds3 = document.createElement('dd');
-            if(result.data[i].identifier == 'p'){
-                as.innerHTML = result.data[i].title;
-                as.setAttribute('href', '/penobrol/'+result.data[i].id);
-                dds1.innerHTML = result.data[i].content;
-                dds1.setAttribute('class', 'ddcontent');
-                if(result.data[i].hashtags.length > 0)
-                {
-                    var temp = '';
-                    for(var h of result.data[i].hashtags)
-                        temp += '#'+h.hash + ' ';
-                    dds2.innerHTML = temp;
-                }
-                dds3.innerHTML =result.data[i].date + ' / ' + result.data[i].view + ' views';
-            }
-            else{
-                as.innerHTML = result.data[i].question;
-                as.setAttribute('href', '/tandya/'+result.data[i].id);
-                dds1.innerHTML = result.data[i].content;
-                dds1.setAttribute('class', 'ddcontent');
-                if(result.data[i].hashtags.length > 0)
-                {
-                    var temp = '';
-                    for(var h of result.data[i].hashtags)
-                        temp += '#'+h.hash + ' ';
-                    dds2.innerHTML = temp;
-                }
-                dds3.innerHTML = result.data[i].date + ' / ' + result.data[i].view + ' views';
-            }
-            dts.appendChild(as);
-            dls.appendChild(dts);
-            dls.appendChild(dds1);
-            dls.appendChild(dds2);
-            dls.appendChild(dds3);
-            lis.appendChild(dls);
-            randomul.appendChild(lis);
-        }
+        result.data.map((data) => {
+            var lis = document.createElement("b-thumbnail");
+            lis.setAttribute("jsonSrc", JSON.stringify(data));
+            return lis;
+        }).forEach((li) => { randomul.appendChild(li); });
   });
 }
