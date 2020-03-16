80bb4a065642df3bef81ed9240aac42ca666d5b1
geniusnam <youngwoo9706@gmail.com>
2020-03-16 10:18:47 +0900

    ADD: Use bootstrap in samusil

--- front/js/samusil.js
+++ front/js/samusil.js
@@ -27,20 +27,3 @@ function makeRequest(method, url, data) {
     });
 }
 
-setInterval(AjaxFiveSec, 1000);
-let countsec = 5;
-function AjaxFiveSec(){
-    if(countsec === 0){
-        countsec=5;
-        makeRequest('get','/api/samusil', null).then((res)=>{
-             res = JSON.parse(res);
-             document.getElementById("users").innerText = ("count user : "+ res.users);
-             document.getElementById("penobrol").innerText = ("count penobrol : "+ res.penobrol);
-             document.getElementById("tandya").innerText = ("count tandya : "+ res.tandya);
-             document.getElementById("youtublog").innerText = ("count youtublog : "+ res.youtublog);
-             document.getElementById("comment").innerText = ("count comment : "+ res.comment);
-             document.getElementById("answer").innerText = ("count answer : "+ res.answer);
-        });
-    }
-    countsec -= 1;
-}
