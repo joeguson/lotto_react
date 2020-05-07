d678d8b7f8a8da29eb2da926b3cb3b7c44c71e3b
spaceship00 <Gus46dl28!>
2020-05-07 17:16:27 +0900

    RFT: Change double quotes to single quote

--- front/js/warnArticle.js
+++ front/js/warnArticle.js
@@ -3,9 +3,9 @@ function __selectWarnFunc(warn_target) {
         let data = {warned_id: warned_id};
         const url = '/api/warn/'+ warn_target;
         makeRequest('POST', url, data)
-            .then(res => {
+            .then(() => {
                 alert("Success warning this " + warn_target);
-            }).catch(res => {
+            }).catch(() => {
             alert("You already warn this " + warn_target);
         })
     }
@@ -14,20 +14,20 @@ function __selectWarnFunc(warn_target) {
 /* ===== POST /{type}/warn ===== */
 /* ===== Article ===== */
 
-warnPenobrol = __selectWarnFunc("penobrol/article");
-warnTandya = __selectWarnFunc("tandya/article");
+warnPenobrol = __selectWarnFunc('penobrol/article');
+warnTandya = __selectWarnFunc('tandya/article');
 warnYoutublog = __selectWarnFunc('youtublog/article');
 
 /* ===== POST /{type}/reply/warn ===== */
 /* ===== Reply ===== */
 
-warnPenobrolCom = __selectWarnFunc("penobrol/reply");
-warnTandyaAns = __selectWarnFunc("tandya/reply");
+warnPenobrolCom = __selectWarnFunc('penobrol/reply');
+warnTandyaAns = __selectWarnFunc('tandya/reply');
 warnYoutublogCom = __selectWarnFunc('youtublog/reply');
 
 /* ===== POST /{type}/re-reply/warn ===== */
 /* ===== Re-reply ===== */
 
-warnPenobrolComCom = __selectWarnFunc("penobrol/rereply");
-warnTandyaAnsCom = __selectWarnFunc("tandya/rereply");
+warnPenobrolComCom = __selectWarnFunc('penobrol/rereply');
+warnTandyaAnsCom = __selectWarnFunc('tandya/rereply');
 warnYoutublogComCom = __selectWarnFunc('youtublog/rereply');
