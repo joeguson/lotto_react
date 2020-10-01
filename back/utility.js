f79a2df3e8fc0358d2b7cc2aa930155c9f1e5a3c
joeguson <joe.guson@beablion.com>
2020-10-01 14:05:49 +0900

    test

--- back/utility.js
+++ back/utility.js
@@ -18,36 +18,47 @@ function compareCombination(comb1, comb2) {
     return JSON.stringify(comb1) === JSON.stringify(comb2);
 }
 
-function getConsequtives(combinationsArr) {
-    const consequtives = new Array(5).fill(0);
+function countConsecutive (combinations) {
+    const consecutives = new Array(5);
     // 0 - 2ea (3times), 1 - 3ea (2times), 2 - 4ea, 3 - 5ea, 4 - 6ea
-    // consequtives[0] = [[], [], []];
-    // consequtives[1] = [[], []];
-    // consequtives[2] = 0;
-    // consequtives[3] = 0;
-    // consequtives[4] = 0;
+    consecutives[0] = [[0], [0], [0]];
+    consecutives[1] = [[0], [0]];
+    consecutives[2] = 0;
+    consecutives[3] = 0;
+    consecutives[4] = 0;
 
-    for (let i = 0; i < combinationsArr.length; i++) {
-        const combination = combinationsArr[i];
+    for (let i = 0; i < combinations.length; i++) {
+        const combination = combinations[i];
         if (combination[5] - combination[0] === 5) {
-            consequtives[4] += 1;
+            consecutives[4]++;
             continue;
         }
-        if (combination[5] - combination[1] === 4 || combination[4]  - combination[0] === 4) {
-            consequtives[3] += 1;
+        if (combination[5] - combination[1] === 4 || combination[4] - combination[0] === 4) {
+            consecutives[3]++;
             continue;
         }
-        if (combination[3] - combination[0] === 3 || combination[4]  - combination[1] === 3 || combination[5] - combination[2] === 3) {
-            consequtives[2] += 1;
+        if (combination[3] - combination[0] === 3 || combination[4] - combination[1] === 3 || combination[5] - combination[2] === 3) {
+            consecutives[2]++;
             continue;
         }
-        if (combination[2] - combination[0] === 2 || combination[3]  - combination[1] === 2 || combination[4] - combination[2] === 2 || combination[5] - combination[3] === 2) {
-            consequtives[1] += 1;
+        if (combination[2] - combination[0] === 2 || combination[3] - combination[1] === 2 || combination[4] - combination[2] === 2 || combination[5] - combination[3] === 2) {
+            let count = 0;
+            if (combination[2] - combination[0] === 2) count++;
+            if (combination[3] - combination[1] === 2) count++;
+            if (combination[4] - combination[2] === 2) count++;
+            if (combination[5] - combination[3] === 2) count++;
+            consecutives[1][count - 1]++;
             continue;
         }
-        if (combination[1] - combination[0] === 1 || combination[2]  - combination[1] === 1 || combination[3] - combination[2] === 1 || combination[4] - combination[3] === 1 || combination[5] - combination[4] === 1) {
-            consequtives[0] += 1;
+        if (combination[1] - combination[0] === 1 || combination[2] - combination[1] === 1 || combination[3] - combination[2] === 1 || combination[4] - combination[3] === 1 || combination[5] - combination[4] === 1) {
+            let count = 0;
+            if (combination[1] - combination[0] === 1) count++;
+            if (combination[2] - combination[1] === 1) count++;
+            if (combination[3] - combination[2] === 1) count++;
+            if (combination[4] - combination[3] === 1) count++;
+            if (combination[5] - combination[4] === 1) count++;
+            consecutives[0][count - 1]++;
         }
     }
-    return consequtives;
+    return consecutives;
 }
--- lotto.js
+++ lotto.js
@@ -3,16 +3,14 @@ const app = express();
 const cookieParser = require('cookie-parser');
 const config =require('./config.json');
 const useragent = require('express-useragent');
+const path = require('path');
 const fs = require('fs');
 
 app.use(useragent.express());
-
+app.use(express.static(path.join(__dirname, './front/build')));
 app.use(express.json({ limit : "50mb" }));
 app.use(express.urlencoded({ limit:"50mb", extended: false }));
-
 app.locals.pretty = true;
-
-app.use(express.static('front'));
 app.use(express.static('back'));
 app.set('views', './front/html');
 app.set('case sensitive routing', false);
@@ -24,33 +22,9 @@ exports.poolConfig = poolConfig;
 const cari = require('./back/cari');
 const service = require('./service/serviceUtil');
 
-fs.readFile('../lotto_combination/lotto_history.txt', 'utf8', async function (err, data){
-    const arr = data.split('\n').map(e => e.trim().split(','));
-
-    arr.forEach(e => {
-        e[0] = parseInt(e[0]);
-        e[1] = e[1].replace('.', '-');
-        e[2] = parseInt(e[2]);
-        e[3] = parseInt(e[3]);
-        e[4] = parseInt(e[4]);
-        e[5] = parseInt(e[5]);
-        e[6] = parseInt(e[6]);
-        e[7] = parseInt(e[7]);
-    });
-
-    arr.forEach(e => {
-        e[1] = e[1].replace('.', '-');
-    });
-    arr.reverse();
-
-    console.log(arr.length);
-    let count = 0;
-
-    for(let i = 900; i < arr.length; i++) {
-        count++;
-    }
-    console.log('=======');
-});
+// fs.readFile('../lotto_combination/lotto_history.txt', 'utf8', async function (err, data){
+//     const arr = data.split('\n').map(e => e.trim().split(','));
+// });
 
 /* ===== router ===== */
 app.use('/cari', cari);
